950ce7b24e8a52155e585b580a47066ef554f73f
Aditya518 <87081712+Aditya518@users.noreply.github.com>
2025-05-01 23:33:11 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Baymax',
+    version= '0.0.0',
+    author= 'Aditya Raj',
+    author_email= 'adityaiitg518@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
